10189173255d5cb9ab7fc7146e110bd16aa31cb5
Malvarez4204 <106292326+Malvarez4204@users.noreply.github.com>
2022-07-18 20:56:03 -0500

    FIRSTPROJEC

---
+++ First Projec/First Projec/First_ProjecApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_ProjecApp.swift
+//  First Projec
+//
+//  Created by MAC on 09/07/22.
+//
+
+import SwiftUI
+
+@main
+struct First_ProjecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
